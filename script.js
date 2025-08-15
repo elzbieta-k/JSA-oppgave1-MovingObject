9823d0d8b58312449f694a077f36ea2e803e8c27
Elzbieta Kukulka <elzbieta.kodehode@gmail.com>
2025-08-15 12:02:30 +0200

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Getting elements from HTML
 const object = document.querySelector(".object");
 const alien1 = document.querySelector(".alien1");
 const alien2 = document.querySelector(".alien2");
@@ -7,16 +8,22 @@ const aliens = [alien1, alien2, alien3];
 
 const message = document.querySelector("h3");
 
+//Setting starting position of the object to 0
 let startingPositionX = 0;
 let startingPositionY = 0;
 
+//Getting the dimensions of the object element in pixels
 const objectHeight = object.offsetHeight;
 const objectWidth = object.offsetWidth;
+
+//Seting every move with arrow to 10
 const move = 10;
 
+//Getting height and width of the viewport
 const windowInnerHeight = document.documentElement.clientHeight;
 const windowInnerWidth = document.documentElement.clientWidth;
 
+//Function that set display of alien element to block and give them random position
 const init = () => {
   aliens.forEach((alien) => {
     alien.style.display = "block";
@@ -25,12 +32,16 @@ const init = () => {
   });
 };
 
+//Function that check is "ufo" is getting an alien
 const checkCollisions = () => {
+  //Getting information about the size of an "ufo" and its position relative to the viewport.
   const ufoRect = object.getBoundingClientRect();
 
+  //Getting information about the size of  every "alien" and its position relative to the viewport.
   for (let alien of aliens) {
     const alienRect = alien.getBoundingClientRect();
 
+    //Getting true or false if the collision happen
     const collision = !(
       ufoRect.top > alienRect.bottom ||
       ufoRect.bottom < alienRect.top ||
@@ -38,25 +49,33 @@ const checkCollisions = () => {
       ufoRect.right < alienRect.left
     );
 
+    //Setting display to none of current alien if the collision is true
     if (collision) {
       alien.style.display = "none";
     }
+
+    //Setting display to none of current alien when clicking on it
     alien.addEventListener("click", () => {
       alien.style.display = "none";
     });
   }
 
+  //Checking if all aliens have changed display to "none" by function .every(), return true or false
   const allHidden = aliens.every((alien) => alien.style.display === "none");
 
+  //If all aliens have display: none - initialize the game (set random positions to alien, and setting display to "block")
   if (allHidden) {
     init();
   }
 };
 
+// Event listener on "click"
 document.addEventListener("click", (e) => {
+  //setting position of "ufo" to clicked position (subtract the half of the size of ufo so it is centered)
   startingPositionX = e.clientX - objectHeight * 0.5;
   startingPositionY = e.clientY - objectWidth * 0.5;
 
+  //Chcecking positions to make sure that "ufo" doesn't leave the screen
   if (startingPositionX < 0) {
     startingPositionX = 0;
   } else if (startingPositionX + objectWidth > windowInnerWidth) {
@@ -69,12 +88,14 @@ document.addEventListener("click", (e) => {
     startingPositionY = windowInnerHeight - objectHeight;
   }
 
+  //Update the position and checking if there is a collision
   object.style.top = startingPositionY + "px";
   object.style.left = startingPositionX + "px";
   checkCollisions();
 });
 
 document.addEventListener("keydown", (e) => {
+  //Chcecking positions to make sure that "ufo" doesn't leave the screen
   if (
     e.key === "ArrowDown" &&
     startingPositionY + move + objectHeight <= windowInnerHeight
@@ -91,9 +112,11 @@ document.addEventListener("keydown", (e) => {
     startingPositionX += move;
   }
 
+  //Update the position and checking if there is a collision
   object.style.top = startingPositionY + "px";
   object.style.left = startingPositionX + "px";
   checkCollisions();
 });
 
+//Initialize the game when page is loaded
 window.onload = init;
