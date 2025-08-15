11035e3d6aa5876d87124e010fed272325722f88
Elzbieta Kukulka <elzbieta.kodehode@gmail.com>
2025-08-15 09:06:21 +0200

    Added checkCollisions function, comments

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const object = document.querySelector(".object");
+const alien1 = document.querySelector(".alien1");
+const alien2 = document.querySelector(".alien2");
+const alien3 = document.querySelector(".alien3");
+
+const aliens = [alien1, alien2, alien3];
+
+const message = document.querySelector("h3");
 
 let startingPositionX = 0;
 let startingPositionY = 0;
@@ -7,10 +14,44 @@ const objectHeight = object.offsetHeight;
 const objectWidth = object.offsetWidth;
 const move = 10;
 
-// const windowInnerHeight = window.innerHeight;
-// const windowInnerWeight = window.innerWidth;
 const windowInnerHeight = document.documentElement.clientHeight;
-const windowInnerWeight = document.documentElement.clientWidth;
+const windowInnerWidth = document.documentElement.clientWidth;
+
+const init = () => {
+  aliens.forEach((alien) => {
+    alien.style.display = "block";
+    alien.style.top = Math.floor(Math.random() * windowInnerHeight) + "px";
+    alien.style.left = Math.floor(Math.random() * windowInnerWidth) + "px";
+  });
+};
+
+const checkCollisions = () => {
+  const ufoRect = object.getBoundingClientRect();
+
+  for (let alien of aliens) {
+    const alienRect = alien.getBoundingClientRect();
+
+    const collision = !(
+      ufoRect.top > alienRect.bottom ||
+      ufoRect.bottom < alienRect.top ||
+      ufoRect.left > alienRect.right ||
+      ufoRect.right < alienRect.left
+    );
+
+    if (collision) {
+      alien.style.display = "none";
+    }
+    alien.addEventListener("click", () => {
+      alien.style.display = "none";
+    });
+  }
+
+  const allHidden = aliens.every((alien) => alien.style.display === "none");
+
+  if (allHidden) {
+    init();
+  }
+};
 
 document.addEventListener("click", (e) => {
   startingPositionX = e.clientX - objectHeight * 0.5;
@@ -18,8 +59,8 @@ document.addEventListener("click", (e) => {
 
   if (startingPositionX < 0) {
     startingPositionX = 0;
-  } else if (startingPositionX + objectWidth > windowInnerWeight) {
-    startingPositionX = windowInnerWeight - objectWidth;
+  } else if (startingPositionX + objectWidth > windowInnerWidth) {
+    startingPositionX = windowInnerWidth - objectWidth;
   }
 
   if (startingPositionY < 0) {
@@ -30,6 +71,7 @@ document.addEventListener("click", (e) => {
 
   object.style.top = startingPositionY + "px";
   object.style.left = startingPositionX + "px";
+  checkCollisions();
 });
 
 document.addEventListener("keydown", (e) => {
@@ -44,17 +86,14 @@ document.addEventListener("keydown", (e) => {
     startingPositionX -= move;
   } else if (
     e.key === "ArrowRight" &&
-    startingPositionX + move + objectWidth <= windowInnerWeight
+    startingPositionX + move + objectWidth <= windowInnerWidth
   ) {
     startingPositionX += move;
   }
 
-  // if (startingPositionX > window.innerWidth || startingPositionX < 0) {
-  //   alert("You reached the wall");
-  // } else if (startingPositionY > window.innerHeight || startingPositionY < 0) {
-  //   alert("You reached the top or bottom");
-  // }
-
   object.style.top = startingPositionY + "px";
   object.style.left = startingPositionX + "px";
+  checkCollisions();
 });
+
+window.onload = init;
